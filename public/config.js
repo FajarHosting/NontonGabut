a7792af9d87bed81c6efcdc288fbdb0b2c8991f6
FajarHosting <82355656+FajarHosting@users.noreply.github.com>
2025-12-25 23:15:37 +0800

    Delete public directory

--- public/config.js
+++
@@ -1,3 +0,0 @@
-// Jika frontend di GitHub Pages, isi API_BASE dengan domain Vercel Anda.
-// Contoh: export const API_BASE = "https://nontongabut.vercel.app";
-export const API_BASE = "https://nonton-gabut.vercel.app";
