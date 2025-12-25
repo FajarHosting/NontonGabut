63e6afdbd1c8ea7c3a210e13c4b0a9f8060d3d4d
FajarHosting <82355656+FajarHosting@users.noreply.github.com>
2025-12-25 23:15:15 +0800

    Delete api directory

--- api/public/payment-info.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-  return res.json({
-    ok: true,
-    dana: process.env.PAY_DANA_NUMBER || "",
-    seabank: process.env.PAY_SEABANK_NUMBER || "",
-    qrisImage: process.env.PAY_QRIS_IMAGE_URL || ""
-  });
-}
