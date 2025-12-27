ab7fe401d1ce939aaa3909b07a37217b39c46c52
FajarHosting <82355656+FajarHosting@users.noreply.github.com>
2025-12-27 12:27:27 +0800

    Add files via upload

--- src/db.js
+++ src/db.js
@@ -1,11 +1,21 @@
 const mongoose = require("mongoose");
 
+let cached = global.__MONGO_CONN__;
+if (!cached) cached = global.__MONGO_CONN__ = { conn: null, promise: null };
+
 async function connectDB() {
   const uri = process.env.MONGODB_URI;
-  if (!uri) throw new Error("MONGODB_URI missing in .env");
-  mongoose.set("strictQuery", true);
-  await mongoose.connect(uri);
-  console.log("MongoDB connected");
+  if (!uri) throw new Error("MONGODB_URI missing");
+
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    mongoose.set("strictQuery", true);
+    cached.promise = mongoose.connect(uri).then((m) => m);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
 
 module.exports = { connectDB };
